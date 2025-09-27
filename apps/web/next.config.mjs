049c1e1a59c9ca113b1c772cca2cb9b0b2bfcd47
lulabtechnology <131550504+lulabtechnology@users.noreply.github.com>
2025-09-27 16:54:02 -0600

    Create next.config.mjs

---
+++ apps/web/next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: { typedRoutes: true },
+  output: "standalone",
+};
+
+export default nextConfig;
