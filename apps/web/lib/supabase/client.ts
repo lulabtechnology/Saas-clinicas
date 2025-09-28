7878562f53c57da80c3c2e1ac418dd27340272b5
lulabtechnology <131550504+lulabtechnology@users.noreply.github.com>
2025-09-27 21:40:12 -0600

    Update client.ts

--- apps/web/lib/supabase/client.ts
+++ apps/web/lib/supabase/client.ts
@@ -1,9 +1,9 @@
 "use client";
 
-import { createClient } from "@supabase/supabase-js";
+import { createBrowserClient } from "@supabase/ssr";
 
 export function createSupabaseBrowser() {
-  return createClient(
+  return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
